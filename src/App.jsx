14037437617bd1f090f4744b43430cebe8e7dcdb
melgeller <melgeller.foto@gmail.com>
2022-07-07 12:37:14 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import './App.scss';
 import NavBar from './components/Static/NavBar';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { useState } from 'react';
 
 function App() {
+  
   return (
     <div className="App">
       <NavBar></NavBar>
--- src/components/Static/NavBar.jsx
+++ src/components/Static/NavBar.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from "react";
+import './NavBar.scss'
+import { Link } from 'react-router-dom'
+
 
 const NavBar = () => {
   return (
     <div className='navbar'>
-    <img src="../../../public/assets/logoperro.png" alt="logoperro"/>
-
+      <img src='../../assets/logoperro.png' alt='logoperro' />
+      <nav className='lista'>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/adopta'>Adopta</Link></li>
+          <li><Link to='/about'>Quienes somos</Link></li>
+          <li><Link to='/blog'>Noticias</Link></li>
+          <li><Link to='/contact'>Contacto</Link></li>
+          <li><Link to='/donations'>Donaciones</Link></li>
+          <li><Link to='/admin'>Administración</Link></li>
+        </ul>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
