b34f9c6ba944c919d2082161c3b66df3d685fb2d
melgeller <melgeller.foto@gmail.com>
2022-07-07 14:32:12 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import './App.scss';
 import NavBar from './components/Static/NavBar';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import { useState } from 'react';
+import Footer from './components/Static/Footer';
 
 function App() {
   
   return (
     <div className="App">
+      <Router>
       <NavBar></NavBar>
+      <Footer></Footer>
+      </Router>
     </div>
   );
 }
