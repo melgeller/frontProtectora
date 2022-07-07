74b89057d62cef9ef5976cfd1e557029d020011b
melgeller <melgeller.foto@gmail.com>
2022-07-07 12:19:07 +0200

    logoperro

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.scss';
+import NavBar from './components/Static/NavBar';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar></NavBar>
     </div>
   );
 }
---
+++ src/components/Static/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+    <img src="../../../public/assets/logoperro.png" alt="logoperro"/>
+
+    </div>
+  )
+}
+
+export default NavBar
