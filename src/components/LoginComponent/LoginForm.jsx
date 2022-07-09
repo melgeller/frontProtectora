5d555618089d60d6b94c708e4c294871016c650e
unknown <garcia.arencon@bootcamp-upgrade.com>
2022-07-09 20:28:15 +0200

    feat

--- src/components/LoginComponent/LoginForm.jsx
+++ src/components/LoginComponent/LoginForm.jsx
@@ -3,6 +3,7 @@ import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import { JwtContext } from '../../context/jwtContext';
 import { API } from '../../services/api';
+import "./LoginForm.scss"
 
 const LoginForm = () => {
     const { setJwt } = useContext(JwtContext);
@@ -19,25 +20,29 @@ const LoginForm = () => {
         });
       };
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-        <div className='formField email'>
-          <label htmlFor='email'>Email:</label>
+      <div class='login'>
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit(onSubmit)}>
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" htmlFor='email'>Email:</label>
           <input
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             type='email'
             id='email'
             {...register('email', { required: true })}
           />
         </div>
-        <div className='formField password'>
-          <label htmlFor='password'>Password:</label>
+        <div class="mb-6">
+          <label class="block text-gray-700 text-sm font-bold mb-2" htmlFor='password'>Password:</label>
           <input
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             type='password'
             id='password'
             {...register('password', { required: true })}
           />
         </div>
-        <button type='submit'>Login</button>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type='submit'>Login</button>
       </form>
+      </div>
   )
 }
 
