c4e9b5dd21e23c3748ff977cf26289cf7403c131
unknown <garcia.arencon@bootcamp-upgrade.com>
2022-07-09 20:46:50 +0200

    feat form

--- src/components/LoginComponent/LoginForm.jsx
+++ src/components/LoginComponent/LoginForm.jsx
@@ -3,7 +3,6 @@ import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import { JwtContext } from '../../context/jwtContext';
 import { API } from '../../services/api';
-import "./LoginForm.scss"
 
 const LoginForm = () => {
     const { setJwt } = useContext(JwtContext);
@@ -20,29 +19,25 @@ const LoginForm = () => {
         });
       };
     return (
-      <div class='login'>
-        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit(onSubmit)}>
-        <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" htmlFor='email'>Email:</label>
+        <form onSubmit={handleSubmit(onSubmit)}>
+        <div className='formField email'>
+          <label htmlFor='email'>Email:</label>
           <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             type='email'
             id='email'
             {...register('email', { required: true })}
           />
         </div>
-        <div class="mb-6">
-          <label class="block text-gray-700 text-sm font-bold mb-2" htmlFor='password'>Password:</label>
+        <div className='formField password'>
+          <label htmlFor='password'>Password:</label>
           <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             type='password'
             id='password'
             {...register('password', { required: true })}
           />
         </div>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type='submit'>Login</button>
+        <button type='submit'>Login</button>
       </form>
-      </div>
   )
 }
 
