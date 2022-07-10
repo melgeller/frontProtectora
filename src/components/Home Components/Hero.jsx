2f2091e005590bc529593569087ed232705016fc
Esther <ev@MacBook-Air-de-Esther.local>
2022-07-10 17:05:43 +0200

    hom1

--- src/components/Home Components/Hero.jsx
+++ src/components/Home Components/Hero.jsx
@@ -2,18 +2,27 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './Hero.scss';
 
+
+
 const Hero = () => {
   return (
-  <section className='hero'>
-  
-   <h1>!UPGRADE HUB ADOPCIONES!</h1>
-   <p>Somos una protectora sin animo de lucr que se fundo en el añp 2010.Surgimos como respuesta al elevado número de animales que son abandonados y/o maltratados a diario en este país y como una alternativa útil y humanitaria para mejorar la situación actual.<br/>Nuestro trabajo se centra en la protección y defensa de los animales en un sentido amplio, tanto a corto plazo, proporcionando un hogar a los animales abandonados, como a largo plazo, difundiendo una cultura y educación basadas en el conocimiento y respeto hacia los animales y fomentando una tenencia responsable de cualquier ser vivo.</p>
-  
-  <img src='../assets/perritoHero.jpeg' alt='perrequeHero' />
+  <section className='homepri'>
+     <h1 className='titulo'>!UPGRADE HUB ADOPCIONES!</h1>
+       <div className='second'>
+   <img src='../assets/perritoHero.jpeg' alt='perrequeHero' />
+   <p className='ph'>Somos una protectora sin animo de lucro que se fundo en el añp 2010.Surgimos como respuesta al elevado número de animales que son abandonados y/o maltratados a diario en este país y como una alternativa útil y humanitaria para mejorar la situación actual.<br/>Nuestro trabajo se centra en la protección y defensa de los animales en un sentido amplio, tanto a corto plazo, proporcionando un hogar a los animales abandonados, como a largo plazo, difundiendo una cultura y educación basadas en el conocimiento y respeto hacia los animales y fomentando una tenencia responsable de cualquier ser vivo.</p>
 
-  <p>¿Nos ayudas? <button className='button'><Link to='/donaciones'> €€€ </Link></button></p>
+  </div>
+  
+  
+<div className='secondhome'>
+<p>Si quieres aportar tu granito de arena, dale aqui --> <button className='buttonh'><Link to='/donaciones'>Colabora</Link></button></p>
 
-  <button className='button'><Link to='/about'> Lee mas sobre nosotros </Link></button>
+<button className='buttonh'><Link to='/about'> Lee mas sobre nosotros </Link></button>
+
+</div>
+
+ 
     
   </section>
   )
