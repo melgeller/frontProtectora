73aa3f70898e29d14b9bfc487e0372eb49ca3508
lucaslosantos <lucaslosantosoliva@gmail.com>
2022-07-11 17:27:11 +0200

    componente NavLinks para el responsive del Navbar

--- src/components/Static/NavBar.jsx
+++ src/components/Static/NavBar.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import './NavBar.scss';
-import { JwtContext } from '../../context/jwtContext';
-import { useContext } from 'react';
-import ButtonLogout from '../LoginComponent/ButtonLogout';
 import SideBar from '../Navbar/SideBar';
-import { NavLink } from 'react-router-dom';
+import NavLinks from './NavLinks';
 
 const NavBar = () => {
-  const { jwt } = useContext(JwtContext);
-  const activeClassName = 'selected';
   return (
     <div className='navbar' id='outer-container'>
       <SideBar
@@ -18,88 +13,7 @@ const NavBar = () => {
       />
       <img src='../../assets/logoperro.png' alt='logoperro' />
       <nav className='lista'>
-        <ul>
-          <li>
-            <NavLink
-              to='/'
-              className={({ isActive }) =>
-                isActive ? activeClassName : undefined
-              }>
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/gallery'
-              className={({ isActive }) =>
-                isActive ? activeClassName : undefined
-              }>
-              Adopta
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/about'
-              className={({ isActive }) =>
-                isActive ? activeClassName : undefined
-              }>
-              Quienes somos
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/blog'
-              className={({ isActive }) =>
-                isActive ? activeClassName : undefined
-              }>
-              Noticias
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/contact'
-              className={({ isActive }) =>
-                isActive ? activeClassName : undefined
-              }>
-              Contacto
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/donaciones'
-              className={({ isActive }) =>
-                isActive ? activeClassName : undefined
-              }>
-              Donaciones
-            </NavLink>
-          </li>
-          {jwt && (
-            <>
-              <li>|</li>
-              <li>
-                <NavLink
-                  to='/adminblog'
-                  className={({ isActive }) =>
-                    isActive ? activeClassName : undefined
-                  }>
-                  Blog
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to='/adminmascota'
-                  className={({ isActive }) =>
-                    isActive ? activeClassName : undefined
-                  }>
-                  Mascotas
-                </NavLink>
-              </li>
-              <li>
-                <ButtonLogout></ButtonLogout>
-              </li>
-            </>
-          )}
-        </ul>
+         <NavLinks></NavLinks>
       </nav>
     </div>
   );
---
+++ src/components/Static/NavLinks.jsx
@@ -0,0 +1,99 @@
+
+import React from 'react';
+import './NavBar.scss';
+import { JwtContext } from '../../context/jwtContext';
+import { useContext } from 'react';
+import ButtonLogout from '../LoginComponent/ButtonLogout';
+import SideBar from '../Navbar/SideBar';
+import { NavLink } from 'react-router-dom';
+
+const NavLinks = () => {
+  const { jwt } = useContext(JwtContext);
+  const activeClassName = 'selected';
+    return (
+        <ul>
+          <li>
+            <NavLink
+              to='/'
+              className={({ isActive }) =>
+                isActive ? activeClassName : undefined
+              }>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='/gallery'
+              className={({ isActive }) =>
+                isActive ? activeClassName : undefined
+              }>
+              Adopta
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='/about'
+              className={({ isActive }) =>
+                isActive ? activeClassName : undefined
+              }>
+              Quienes somos
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='/blog'
+              className={({ isActive }) =>
+                isActive ? activeClassName : undefined
+              }>
+              Noticias
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='/contact'
+              className={({ isActive }) =>
+                isActive ? activeClassName : undefined
+              }>
+              Contacto
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to='/donaciones'
+              className={({ isActive }) =>
+                isActive ? activeClassName : undefined
+              }>
+              Donaciones
+            </NavLink>
+          </li>
+          {jwt && (
+            <>
+              <li>|</li>
+              <li>
+                <NavLink
+                  to='/adminblog'
+                  className={({ isActive }) =>
+                    isActive ? activeClassName : undefined
+                  }>
+                  Blog
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to='/adminmascota'
+                  className={({ isActive }) =>
+                    isActive ? activeClassName : undefined
+                  }>
+                  Mascotas
+                </NavLink>
+              </li>
+              <li>
+                <ButtonLogout></ButtonLogout>
+              </li>
+            </>
+          )}
+        </ul>
+  )
+}
+
+export default NavLinks
