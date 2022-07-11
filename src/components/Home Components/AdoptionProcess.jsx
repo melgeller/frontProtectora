0b6c33e32dc870afb47cb0d485450811e1a23cac
Esther <ev@MacBook-Air-de-Esther.local>
2022-07-11 10:40:55 +0200

    varios

--- src/components/Home Components/AdoptionProcess.jsx
+++ src/components/Home Components/AdoptionProcess.jsx
@@ -5,28 +5,23 @@ const AdoptionProcess = () => {
     <section>
       <h2 id="h2">Proceso de adopción</h2>
       <div className='firstdiv'>
-      <ul className='numeracion'>
+      <ul className='numeracionlu'>
         <li>
-          <img src='' alt='' />
-          <div id="circulo"><p id='numcircu'> 1</p></div>
-          <p>Ven a conocernos a la prote y si sientes un match con alguno de nuestros peludos escribenos. </p>
+          <img src='../../assets/numeros/sinfondo1.png' alt='one' />
+          <p>Ven a conocernos a la prote y si sientes mach un con alguno de nuestros peludos escribenos. </p>
         </li>
         <li>
-          <img src='' alt='' />
-          <div id="circulo"><p id='numcircu'> 2</p></div>
+        <img src='../../assets/numeros/el2.png' alt='one' />
+          <p>Después de recibir tu email, estudiaremos tu caso, nos pondremos en contacto con vosotros lo antes posible y organizaremos otra visita para que os podáis seguir conociendo.</p>
+        </li>
+        <li>
+        <img src='../../assets/numeros/el3.png' alt='one' />
           
-          <p>Después de recibir tu email, estudiaremos tu caso, nos pondremos en contacto con vosotros lo antes posible y organizaremos otra visita para que os podais seguir conociendo.  </p>
+          <p>Si entre vosotos existe una conexión y cumpleslos requisitos imprescincibles para nosotros y estás de acuerdo con las condiciones de la protectora ya solo queda una cosa...</p>
         </li>
         <li>
-          <img src='' alt='' />
-          <div id="circulo"><p id='numcircu'> 3</p></div>
-          <p>Si entre vosotos existe una conexión  y pasais los requesitos impresdincibles para nosotros y todos estamos de acuerdo con las condiciones de la protectora ya solo queda una cosa...</p>
-        </li>
-        <li>
-          <img src='' alt='' />
-          <div id="circulo"><p id='numcircu'> 4</p></div>
-          
-          <p>Organizar el dia perfecto para venir a recoger al nuevo miembro de la familiay disfrutar toda la vida de él.</p>
+        <img src='../../assets/numeros/el4.png' alt='one' />
+        <p>Organizar el dia perfecto para venir a recoger al nuevo miembro de la familia y disfrutar toda la vida de él.</p>
         </li>
       </ul>
       </div>
